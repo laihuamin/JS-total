380e6b570d556f01f830148278f1d327c9b4d258
laihuamin <1445509994@qq.com>
2018-12-22 18:07:50 +0800

    提交创建背景

--- hilo/flybird/js/game.js
+++ hilo/flybird/js/game.js
@@ -1,14 +1,17 @@
 (function() {
+    window.onload = function() {
+        game.init();
+    }
     var game = window.game = {
-        asset = null,
-        width = 0,
-        height = 0,
-        scale = 0,
+        width: 0,
+        height: 0,
+        scale: 0,
         init: function() {
             this.asset = new game.asset();
             this.asset.on('complete', function(e) {
                 this.asset.off('complete');
                 this.initStage();
+                this.initBackground();
             }.bind(this));
             this.asset.load();
         },
@@ -17,16 +20,42 @@
             this.height = 1280;
             this.scale = 0.5;
             // 舞台
-            this.stage = new HTMLDialogElement.Stage({
+            this.stage = new Hilo.Stage({
                 width: this.width,
                 height: this.height,
                 scaleX: this.scale,
                 scaleY: this.scale
             })
             document.body.appendChild(this.stage.canvas);
+            // 设置定时器
+            this.tick = new Hilo.Ticker(60);
+            // 将舞台元素加入到定时器队列
+            this.tick.addTick(this.stage);
+            // 启动定时器
+            this.tick.start();
+
+        },
+        // 设置游戏背景
+        initBackground: function() {
+            var bgWidth = this.width * this.scale;
+            var bgHeight = this.height * this.scale;
+            document.body.insertBefore(Hilo.createElement('div', {
+                id: 'bg',
+                style: {
+                    position: 'absolute',
+                    background: 'url(images/bg.png) no-repeat',
+                    backgroundSize: bgWidth + 'px, ' + bgHeight + 'px',
+                    width: bgWidth + 'px',
+                    height: bgHeight + 'px' 
+                }
+            }), this.stage.canvas)
+            this.ground = new Hilo.Bitmap({
+                id: 'ground',
+                image: this.asset.ground
+            })
+            this.ground.y = this.height - this.ground.height;
+            Hilo.Tween.to(this.ground, {x: -60}, {duration:300, loop:true})
+            this.tick.addTick(Hilo.Tween)
         }
-    }
-    window.onload = function() {
-        game.init();
     }
 })()
